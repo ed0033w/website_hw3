6394f0282e408ca34bb3e17e337f3cc961c520dd
ed <103703048@nccu.edu.tw>
2017-04-06 03:44:51 +0800

    this is homework3

---
+++ web-hw3/public/index.js
@@ -0,0 +1,17 @@
+
+function toTop() {
+    var elmnt = document.body;
+    
+    elmnt.scrollTop -= 9998;
+}
+function small(){
+    document.body.style.fontSize="small";
+}
+
+function medium(){
+    document.body.style.fontSize="medium";
+}
+
+function large(){
+    document.body.style.fontSize="x-large";
+}
